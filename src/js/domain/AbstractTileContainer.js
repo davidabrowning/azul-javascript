eb21eb17b5699f20192a35435c3a681a7076ede3
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-13 13:35:31 +0200

    Move indexOf() and claim() from FactoryDisplay to AbstractTileContainer.

--- src/js/domain/AbstractTileContainer.js
+++ src/js/domain/AbstractTileContainer.js
@@ -17,4 +17,26 @@ class AbstractTileContainer {
     size() {
         return this.tiles.length;
     }
+    /**
+     * @param {int} tileValue the Tile.value to search for
+     * @returns int representing first found instance of Tile with tileValue
+     */
+    indexOf(tileValue) {
+        for (let i = 0; i < this.tiles.length; i++) {
+            if (this.tiles[i].value == tileValue) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    claim(tileValue) {
+        let claimedTiles = [];
+        while(this.contains(tileValue)) {
+            let tileIndex = this.indexOf(tileValue);
+            let tile = this.remove(tileIndex);
+            claimedTiles.push(tile);
+        }
+        return claimedTiles;
+    }    
 }
--- src/js/domain/FactoryDisplay.js
+++ src/js/domain/FactoryDisplay.js
@@ -1,14 +1,3 @@
 class FactoryDisplay extends AbstractTileContainer {
-    add(tile) {
-        this.tiles.push(tile);
-    }
-    contains(tileValue) {
-        let foundNeedle = false;
-        this.tiles.forEach(tile => {
-            if(tile.value == tileValue) {
-                foundNeedle = true;
-            }
-        });
-        return foundNeedle;
-    }
+
 }
