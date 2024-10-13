a23bc164ac5a744f7b6c2133f118b1df99fffdc8
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-13 10:18:54 +0200

    Add FactoryCenter.claim().

--- src/js/domain/FactoryCenter.js
+++ src/js/domain/FactoryCenter.js
@@ -22,7 +22,27 @@ class FactoryCenter {
         this.tiles.splice(tileIndex, 1);
         return tile;
     }
-    claim(tileValue) {
 
+    /**
+     * @param {int} tileValue the Tile.value to search for
+     * @returns int representing first found instance of Tile with tileValue
+     */
+    indexOf(tileValue) {
+        for (let i = 0; i < this.tiles.length; i++) {
+            if (this.tiles[i].value == tileValue) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    claim(tileValue) {
+        let claimedTiles = [];
+        while(this.contains(tileValue)) {
+            let tileIndex = this.indexOf(tileValue);
+            let tile = this.remove(tileIndex);
+            claimedTiles.push(tile);
+        }
+        return claimedTiles;
     }
 }
