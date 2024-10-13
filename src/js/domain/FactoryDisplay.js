e247b122febbca01e3bfb64419526b6d5b9ae31b
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-13 15:26:42 +0200

    Add id to FactoryDisplay constructor.

--- src/js/domain/FactoryDisplay.js
+++ src/js/domain/FactoryDisplay.js
@@ -1,3 +1,6 @@
 class FactoryDisplay extends AbstractTileContainer {
-
+    constructor(id) {
+        super();
+        this.id = id;
+    }
 }
--- src/js/logic/Controller.js
+++ src/js/logic/Controller.js
@@ -1,8 +1,13 @@
 class Controller {
     constructor(userInterface) {
         this.userInterface = userInterface;
+        this.game = null;
     }
     startSession() {
         this.userInterface.setController(this);
+        this.game = new Game(2);
+        for (let i = 0; i < this.game.factoryDisplays.length; i++) {
+            // this.userInterface.showFactoryDisplay();
+        }
     }
 }
