e3cbf192d395ce87dff8126bcfdd7099ac991e99
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-13 13:32:30 +0200

    Move FactoryDisplay functionality to AbstractTileContainer class.

--- src/js/domain/AbstractTileContainer.js
+++ src/js/domain/AbstractTileContainer.js
@@ -2,4 +2,16 @@ class AbstractTileContainer {
     constructor() {
         this.tiles = [];
     }
+    add(tile) {
+        this.tiles.push(tile);
+    }
+    contains(tileValue) {
+        let foundNeedle = false;
+        this.tiles.forEach(tile => {
+            if(tile.value == tileValue) {
+                foundNeedle = true;
+            }
+        });
+        return foundNeedle;
+    }
 }
