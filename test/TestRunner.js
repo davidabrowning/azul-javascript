0690094c88cde8969d203367a42f97a5c74503e7
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-13 12:21:12 +0200

    Move test functions to the new individual Test.js files.

--- test/TestRunner.js
+++ test/TestRunner.js
@@ -34,66 +34,6 @@ class TestRunner {
 
     runUnitTests() {
         let testSectionTitle = "";
-        let testTitle = "";
-        let testGame = null;
-        let testFactoryCenter = null;
-
-        testTitle = "Two plus two equals four";
-        this.assertEquals(testTitle, 4, 2 + 2);
-
-        testTitle = "Game bag starts with 100 tiles";
-        testGame = new Game(2);
-        this.assertEquals(testTitle, 100, testGame.tileBag.size());
-
-        testTitle = "Two games should not have the same tiles";
-        testGame = new Game(2);
-        let testGame2 = new Game(2);
-        let foundDifferingTiles = false;
-        for (let i = 0; i < testGame.tileBag.size(); i++) {
-            if (testGame.tileBag.drawTile().value != testGame2.tileBag.drawTile().value) {
-                foundDifferingTiles = true;
-            }
-        }
-        this.assertEquals(testTitle, true, foundDifferingTiles);
-
-        testTitle = "TileBag size after drawing one Tile is 99";
-        testGame = new Game(2);
-        testGame.tileBag.drawTile();
-        this.assertEquals(testTitle, 99, testGame.tileBag.size());
-
-        testTitle = "Factory display starts round with four tiles";
-        testGame = new Game(2);
-        testGame.prepareRound();
-        this.assertEquals(testTitle, 4, testGame.factoryDisplays[2].tiles.length);
-
-        testTitle = "Factory center starts round with zero tiles";
-        testGame = new Game(2);
-        testGame.prepareRound();
-        this.assertEquals(testTitle, 0, testGame.factoryCenter.size());
-
-        testTitle = "Factory center persists discarded tiles";
-        testFactoryCenter = new FactoryCenter();
-        testFactoryCenter.add(new Tile(0));
-        testFactoryCenter.add(new Tile(2));
-        this.assertEquals(testTitle, 2, testFactoryCenter.size());
-
-        testTitle = "FactoryCenter does not contain a Tile that was never discarded";
-        testFactoryCenter = new FactoryCenter();
-        testFactoryCenter.add(new Tile(3));
-        this.assertEquals(testTitle, false, testFactoryCenter.contains(2));
-
-        testTitle = "FactoryCenter does contain a Tile that was discarded";
-        testFactoryCenter = new FactoryCenter();
-        testFactoryCenter.add(new Tile(3));
-        let flag = testFactoryCenter.contains(3);
-        this.assertEquals(testTitle, true, testFactoryCenter.contains(3));
-
-        testTitle = "FactoryDisplay is empty after claiming Tile";
-        testGame = new Game(2);
-        testGame.factoryDisplays[0].tiles[0] = new Tile(0);
-        testGame.factoryDisplays[0].tiles[1] = new Tile(0);
-        testGame.factoryDisplays[0].tiles[2] = new Tile(2);
-        testGame.factoryDisplays[0].tiles[3] = new Tile(2);
 
         testSectionTitle = "FactoryCenter tests";
         this.printTestSectionTitle(testSectionTitle);
@@ -118,7 +58,5 @@ class TestRunner {
         testSectionTitle = "TileBag tests";
         this.printTestSectionTitle(testSectionTitle);
         runTileBagTests(this);
-
-        testTitle = "FactoryCenter contains unclaimed FactoryDisplay Tiles";
     }
 }
