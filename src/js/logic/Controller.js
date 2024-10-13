796d7a3dd1289a87a7dff86002864bd4f900e1dd
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-13 20:59:38 +0200

    Update logic on FactoryDisplay Tile click.

--- src/js/logic/Controller.js
+++ src/js/logic/Controller.js
@@ -46,11 +46,19 @@ class Controller {
     handleFactoryDisplayTileClick(factoryDisplayId, tileNum) {
         // Remove previously selected tiles
         this.userInterface.removeSelectedEffectFromAllTiles();
+        this.game.factoryDisplays.forEach(factoryDisplay => {
+            factoryDisplay.unselect();
+        });
+        this.game.factoryCenter.unselect();
 
         // Select these tiles
         let factoryDisplay = this.game.factoryDisplays[factoryDisplayId];
         let tile = factoryDisplay.tiles[tileNum];
         let tileValue = tile.value;
         this.userInterface.addSelectedEffectFactoryDisplay(factoryDisplayId, tileValue);
+        factoryDisplay.select(tileValue);
+
+        // Update printed instructions
+        this.userInterface.printPlaceTileMessage(this.game.players[this.game.activePlayerNum]);
     }
 }
