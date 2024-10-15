0623a8863f03d9da6babf0459e85b7366f8103be
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-15 09:13:46 +0200

    Add PatternLine.rowMaxCapacity().

--- src/js/domain/PatternLine.js
+++ src/js/domain/PatternLine.js
@@ -1,14 +1,49 @@
 class PatternLine extends AbstractTileContainer {
+    constructor() {
+        super();
+        this.tiles = [ 
+                                    null, 
+                              null, null,
+                        null, null, null,
+                  null, null, null, null,
+            null, null, null, null, null
+        ];
+    }
+    rowIsEmpty(rowNum) {
+        switch(rowNum) {
+            case 0: return this.tiles[0] == null;
+            case 1: return this.tiles[1] == null;
+            case 2: return this.tiles[3] == null;
+            case 3: return this.tiles[6] == null;
+            case 4: return this.tiles[10] == null;
+            default: return true;
+        }
+    }
+    rowMaxCapacity(rowNum) {
+        return rowNum + 1;
+    }
+    rowPlacedTilesNum(rowNum) {
+        
+    }
     place(tileArray, row) {
+        let thisRowAlreadyHasTiles = false;
+        let rejectedTiles = [];
         switch(row) {
             case 0:
                 this.tiles[0] = tileArray[0];
+                break;
+            case 1:
+                break;
             case 2:
                 this.tiles[3] = tileArray[0];
+                break;
+            case 3:
+                break;
+            case 4:
                 break;
             default:
                 break;
         }
-        return [];
+        return rejectedTiles;
     }
 }
--- test/domain/PatternLineTests.js
+++ test/domain/PatternLineTests.js
@@ -17,6 +17,30 @@ function runPatternLineTests(testRunner) {
     testPatternLine.place(testTileArray, 0);
     testRunner.assertEquals(testTitle, 1, testPatternLine.tiles[0].value);
 
+    testTitle = "rowIsEmpty() returns true when no Tiles have been placed";
+    testPatternLine = new PatternLine();
+    testRunner.assertEquals(testTitle, true, testPatternLine.rowIsEmpty(2));
+
+    testTitle = "First row's maxCapacity is 1";
+    testPatternLine = new PatternLine();
+    testRunner.assertEquals(testTitle, 1, testPatternLine.rowMaxCapacity(0));
+
+    testTitle = "Last row's maxCapacity is 5";
+    testPatternLine = new PatternLine();
+    testRunner.assertEquals(testTitle, 5, testPatternLine.rowMaxCapacity(4));
+
+    testTitle = "canPlace() returns false when a Tile of a different type has been placed";
+    testPatternLine = new PatternLine();
+
+    testTitle = "canPlace() returns true when a Tile of this type has been placed";
+    testPatternLine = new PatternLine();
+
+    testTitle = "rowIsEmpty() returns false when Tiles have been placed";
+    testPatternLine = new PatternLine();
+    testTileArray = [new Tile(1), new Tile(1), new Tile(1)];
+    testPatternLine.place(testTileArray, 2);
+    testRunner.assertEquals(testTitle, true, testPatternLine.rowIsEmpty(2));
+
     testTitle = "Attempting to place 3 Tiles on row 0 returns 2 Tiles";
     testPatternLine = new PatternLine();
     testTileArray = [new Tile(1), new Tile(1), new Tile(1)];
