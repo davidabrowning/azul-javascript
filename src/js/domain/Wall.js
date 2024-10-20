7660291de526fb425c6eed40fc352c7392aa8b7d
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-20 12:06:23 +0200

    Add properties for rowSize and colSize.

--- src/js/domain/Wall.js
+++ src/js/domain/Wall.js
@@ -18,5 +18,81 @@ class Wall {
         let targetIndex = row * 5 + col;
         return this.targetTileValueByIndex(targetIndex);
     }
+    targetTileIndexByRow(tileType, row) {
+        let indexCountStart = row * this.rowSize;
+        for (let i = indexCountStart; i < indexCountStart + this.rowSize; i++) {
+            if (this.targets[i] == tileType) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    adjacentTilesLeft(row, col) {
+        let adjacentTileCounter = 0;
+        for (let i = col - 1; i >= 0; i--) {
+            if (this.placedTileValueByRowCol(row, i) == -1) {
+                break;
+            }
+            adjacentTileCounter++;
+        }
+        return adjacentTileCounter;
+    }
+    adjacentTilesRight(row, col) {
+        let adjacentTileCounter = 0;
+        for (let i = col + 1; i < this.rowSize; i++) {
+            if (this.placedTileValueByRowCol(row, i) == -1) {
+                break;
+            }
+            adjacentTileCounter++;
+        }
+        return adjacentTileCounter;
+    }
+    adjacentTilesAbove(row, col) {
+        let adjacentTileCounter = 0;
+        for (let i = row - 1; i >= 0; i--) {
+            if (this.placedTileValueByRowCol(i, col) == -1) {
+                break;
+            }
+            adjacentTileCounter++;
+        }
+        return adjacentTileCounter;
+    }
+    adjacentTilesBelow(row, col) {
+        let adjacentTileCounter = 0;
+        for (let i = row + 1; i < this.colSize; i++) {
+            if (this.placedTileValueByRowCol(i, col) == -1) {
+                break;
+            }
+            adjacentTileCounter++;
+        }
+        return adjacentTileCounter;
+    }
+    calculateIncrementalScore(tileType, row) {
+        let rowScore = 0;
+        let colScore = 0;
+        let totalScore = 0;
 
+        let targetTileIndex = this.targetTileIndexByRow(tileType, row);
+        let col = this.targetTileColByIndex(targetTileIndex);
+
+        rowScore += this.adjacentTilesLeft(row, col);
+        rowScore += this.adjacentTilesRight(row, col);
+        if (rowScore > 0) {  // If there are row points,
+            rowScore++;      // Get a point for the tile itself
+        }
+
+        colScore += this.adjacentTilesAbove(row, col);
+        colScore += this.adjacentTilesBelow(row, col);
+        if (colScore > 0) {  // If there are col points,
+            colScore++;      // Get a point for the tile itself
+        }
+
+        if (rowScore == 0 && colScore == 0) {
+            totalScore = 1;
+        } else {
+            totalScore = rowScore + colScore;
+        }
+
+        return totalScore;
+    }
 }
