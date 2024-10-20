1c2c5388109e7a6dc8464bef76316a0e4ab20411
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-20 10:47:45 +0200

    Build Wall class.

--- src/js/domain/Wall.js
+++ src/js/domain/Wall.js
@@ -1,3 +1,22 @@
-class Wall extends AbstractTileContainer {
+class Wall {
+    constructor() {
+        this.tiles = [  -1, -1, -1, -1, -1,
+                        -1, -1, -1, -1, -1,
+                        -1, -1, -1, -1, -1,
+                        -1, -1, -1, -1, -1,
+                        -1, -1, -1, -1, -1 ];
+        this.targets = [0, 1, 2, 3, 4,
+                        4, 0, 1, 2, 3,
+                        3, 4, 0, 1, 2,
+                        2, 3, 4, 0, 1,
+                        1, 2, 3, 4, 0 ];
+    }
+    targetTileValueByIndex(targetIndex) {
+        return this.targets[targetIndex];
+    }
+    targetTileValueByRowCol(row, col) {
+        let targetIndex = row * 5 + col;
+        return this.targetTileValueByIndex(targetIndex);
+    }
 
 }
