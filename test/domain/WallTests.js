413b7957be70c279d626b9100cf37708ab828ebf
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-20 10:47:35 +0200

    Build initial Wall tests.

--- test/domain/WallTests.js
+++ test/domain/WallTests.js
@@ -1,4 +1,25 @@
 function runWallTests(testRunner) {
     let testTitle = "";
     let testGame = null;
+    testWall = null;
+
+    testTitle = "Target tile value for position 0 is 0";
+    testWall = new Wall();
+    testRunner.assertEquals(testTitle, 0, testWall.targetTileValueByIndex(0));
+
+    testTitle = "Target tile value for position 13 is 1";
+    testWall = new Wall();
+    testRunner.assertEquals(testTitle, 0, testWall.targetTileValueByIndex(24));
+
+    testTitle = "Target tile value for position 16 is 3";
+    testWall = new Wall();
+    testRunner.assertEquals(testTitle, 0, testWall.targetTileValueByIndex(24));
+
+    testTitle = "Target tile value for position 24 is 0";
+    testWall = new Wall();
+    testRunner.assertEquals(testTitle, 0, testWall.targetTileValueByIndex(24));
+
+    testTitle = "Target tile value for position (1, 3) is 2";
+    testWall = new Wall();
+    testRunner.assertEquals(testTitle, 2, testWall.targetTileValueByRowCol(1, 3));
 }
